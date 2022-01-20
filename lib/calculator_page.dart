f856681cffa6863f2d1fde373b8ec39733b77c81
dmbappi.bd@gmail.com <dmbappi.bd@gmail.com>
2022-01-20 20:32:09 +0600

    grid view

--- lib/calculator_page.dart
+++ lib/calculator_page.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
 class CalculatorPage extends StatelessWidget {
-  const CalculatorPage({Key? key}) : super(key: key);
+   CalculatorPage({Key? key}) : super(key: key);
+
+  List buttonsList = [
+    'AC',
+    'DEl',
+    '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1','2','3','+','^','0','.','=',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title:const Text('Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text('0', style: TextStyle(fontSize: 40,),),
+            const Divider(),
+            GridView.builder(
+              shrinkWrap: true,
+              itemCount: buttonsList.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount : 4,
+              ),
 
+
+              itemBuilder: (context, index){
+                return Text('0');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }

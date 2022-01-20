c2b8fb339bf4812c4fc4157bc29ba5bde57a8b2e
dmbappi.bd@gmail.com <dmbappi.bd@gmail.com>
2022-01-20 23:09:25 +0600

    calculator function added

--- lib/calculator_page.dart
+++ lib/calculator_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-class CalculatorPage extends StatelessWidget {
+class CalculatorPage extends StatefulWidget {
    CalculatorPage({Key? key}) : super(key: key);
 
+  @override
+  State<CalculatorPage> createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
   List buttonsList = [
     'AC',
     'DEl',
     '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1','2','3','+','^','0','.','=',
   ];
+
+  String expression = '';
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +28,55 @@ class CalculatorPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text('0', style: TextStyle(fontSize: 40,),),
+            Text(expression, style: TextStyle(fontSize: 40,),),
             const Divider(),
             GridView.builder(
               shrinkWrap: true,
               itemCount: buttonsList.length,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount : 4,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
               ),
 
 
               itemBuilder: (context, index){
-                return Text('0');
+                return GestureDetector(
+                  onTap: (){
+
+                    setState(() {
+                      if(buttonsList[index] == "AC"){
+                        expression = '';
+                      }else if (buttonsList[index] == "DEL"){
+
+                       expression = expression.substring(0,expression.length -1);
+                      }else if (buttonsList[index] == "="){
+                        Parser p = Parser();
+                        Expression parsedExp = p.parse(expression);
+                        ContextModel cm = ContextModel();
+                        double eval =
+                        parsedExp.evaluate(EvaluationType.REAL, cm);
+                        expression = eval.toString();
+                      }
+                      else{
+                        expression = expression + buttonsList[index];
+                      }
+
+                    });
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[800],
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(buttonsList[index], style: TextStyle(
+                        fontSize: 26
+                      ),),
+                    ),
+                  ),
+                );
               },
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         brightness: Brightness.dark,
-
+        canvasColor: Colors.black,
         primarySwatch: Colors.blue,
       ),
       home: CalculatorPage(),
